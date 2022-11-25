17c5681e79cb6221eaa0b2790f15a26197bbf5d2
yu02200059 <yuxiang02200059@gmail.com>
2022-11-25 21:27:13 +0800

    add api sellers

--- pages/api/sellers/[id]/activities.ts
+++ pages/api/sellers/[id]/activities.ts
@@ -3,5 +3,20 @@ import prisma from '../../../../lib/prisma';
 
 // GET /api/sellers/:id/activities (Get the seller's activities)
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-	// TODO:
+	const id = req.query.id;
+	if (req.method === 'GET') {
+		const discount = await prisma.discount.findMany({
+			where: { seller_id: Number(id), format: "ACTIVITY" },
+		});
+		res.json({
+			status: 0,
+			message: "success",
+			data: discount
+		});
+	} else {
+		throw new Error(
+			`The HTTP ${req.method} method is not supported at this route.`
+		);
+	}
 }
+
--- pages/api/sellers/[id]/coupons.ts
+++ pages/api/sellers/[id]/coupons.ts
@@ -3,5 +3,19 @@ import prisma from '../../../../lib/prisma';
 
 // GET /api/sellers/:id/coupons (Get the seller's coupons)
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-	// TODO:
+	const id = req.query.id;
+	if (req.method === 'GET') {
+		const discount = await prisma.discount.findMany({
+			where: { seller_id: Number(id), format: "COUPON" },
+		});
+		res.json({
+			status: 0,
+			message: "success",
+			data: discount
+		});
+	} else {
+		throw new Error(
+			`The HTTP ${req.method} method is not supported at this route.`
+		);
+	}
 }
