beb9effe422ed7591f72021e1de927fccc6048d4
yu02200059 <yuxiang02200059@gmail.com>
2022-11-26 10:24:52 +0800

    modify some api

--- pages/api/buyers/me/coupons/[id].ts
+++ pages/api/buyers/me/coupons/[id].ts
@@ -1,9 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { type } from 'os';
+import { startTransition } from 'react';
+import { getEnvironmentData } from 'worker_threads';
 import prisma from '../../../../../lib/prisma';
 
 // POST /api/buyers/me/coupons/:id (Create coupon)
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 	// TODO:
+<<<<<<< Updated upstream
 	if (req.method === 'POST') {
 		const { name, scope, type, value, amount, startAt, endAt, productId, id } = req.query;
 		const result = await prisma.discount.create({
@@ -34,4 +38,46 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
 			`The HTTP ${req.method} method is not supported at this route.`
 		);
 	}
+=======
+	{
+		if (req.method === 'POST') {
+			const { id, name, scope, type, value, amout, startAt, endAt, productId, sellerId } = req.query;
+			const result = await prisma.discount.create({
+				data: {
+					id: Number(id),
+					name: String(name),
+					scope: String(scope),
+					type: String(type),
+					value: Number(value),
+					amount: Number(amount),
+					startAt: String(startAt),
+					endAt: String(endAt),
+					productId: Number(productId),
+					sellerId: String(sellerId)
+				},
+			});
+			res.json({
+				status: 0,
+				message: "success",
+				data: {
+					id: result.id,
+					name: result.name,
+					scope: result.scope,
+					type: result.type,
+					value: result.value,
+					amount: result.amount,
+					startAt: result.startAt,
+					endAt: result.endAt,
+					productId: result.productId,
+					sellerId: result.sellerId
+				}
+			});
+	
+		} else {
+			throw new Error(
+				`The HTTP ${req.method} method is not supported at this route.`
+			);
+		}
+}
+>>>>>>> Stashed changes
 }
--- pages/api/sellers/me/coupons.ts
+++ pages/api/sellers/me/coupons.ts
@@ -5,4 +5,44 @@ import prisma from '../../../../lib/prisma';
 // GET  /api/sellers/me/coupons (Get my coupons)
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 	// TODO:
+	{
+		if (req.method === 'POST') {
+			const { id, name, scope, type, value, amount, startAt, endAt, productId, sellerId } = req.query;
+			if (productId === null) {
+			const result = await prisma.discount.create({
+				data: {
+					// FIXIT:
+					name: String(name),
+					format: String("COUPON"),
+					scope: String(scope),
+					type: String(type),
+					value: Number(value),
+					amount: Number(amount),
+					startAt: String(startAt),
+					endAt: String(endAt),
+					// sellerId: Number(sellerId) 
+				},
+			});
+			res.json({
+				status: 0,
+				message: "success",
+				data: {
+					id: result.id,
+					name: result.name,
+					scope: result.scope,
+					type: result.type,
+					value: result.value,
+					amount: result.amount,
+					startAt: result.startAt,
+					endAt: result.endAt,
+					sellerId: result.sellerId
+				}
+			});
+		}	
+		} else {
+			throw new Error(
+				`The HTTP ${req.method} method is not supported at this route.`
+			);
+		}
+}
 }
